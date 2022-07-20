32d30bd86d7dfa03aa6d21f1435f68a9364359d9
zeke800 <82127189+zeke800@users.noreply.github.com>
2022-07-19 22:36:49 -0400

    Add IR blend

--- makeRGB.py
+++ makeRGB.py
@@ -11,8 +11,8 @@ except:
     args.append("-noir")
 
 #images
-ch2 = Image.open("cha_5.jpg").convert("L")
-ch4 = Image.open("chb_5.jpg").convert("L")
+ch2 = Image.open("chb_4.png").convert("L")
+ch4 = Image.open("cha_4.png").convert("L")
 
 #ch4 = PIL.ImageOps.invert(ch4)
 #ch4 = PIL.ImageOps.autocontrast(ch4)
@@ -20,6 +20,7 @@ ch2 = PIL.ImageOps.autocontrast(ch2)
 
 #Variables
 deyellowfactor = 100
+black_pixels = []
 
 #convert to arrays
 ch2_array = ch2.load()
@@ -31,12 +32,6 @@ xsize,ysize = ch2.size
 #output
 outimg = Image.new('RGB',(xsize,ysize))
 #main loop
-"""
-for y in range(ysize):
-    for x in range(xsize):
-        
-        ch4.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
-"""
 if args[1] == "-ir":
     for y in range(ysize):
         for x in range(xsize):
@@ -50,14 +45,4 @@ else:
             outimg.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
 
 outimg = PIL.ImageOps.autocontrast(outimg)
-"""       
-#Part 2, get rid of nasty yellow tinge
-outimgarray = outimg.load()
-outimg_corr = Image.new('RGB',(xsize,ysize))
-
-for y in range(ysize):
-    for x in range(xsize):
-        if outimgarray[x,y] == (182,182,29):
-            print("YELLOW")
-        
-"""
+outimg.save("color.png")
