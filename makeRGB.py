f0f59d388bfd93a0bb61010d4a93da53b580d7fc
Edoardo Riggio <edo.riggio19@gmail.com>
2023-08-01 20:45:23 +0200

    adds help and argument recognition for makeRGB.py

--- makeRGB.py
+++ makeRGB.py
@@ -1,102 +1,106 @@
-#Zeke800's APT colorizer
-#Convert APT images to color (224 composition)
-from PIL import Image
-import PIL.ImageOps
-import sys
+#!/usr/bin/env python3
 
-args = list(sys.argv)
+import argparse
 
-try:
-    args[1]
-except:
-    args.append("-noir")
+from array import array
+from PIL import Image, ImageOps
 
-try:
-    args[2]    
 
-except:
-    if args[1] == "-boost":
-        args.append("-boost")
-    else:
-        args.append("-noboost")
+# Constants
+DEYELLOW_FACTOR = 100
+CH2_BLEND = 1
+CH4_BLEND = 200
+CH2_BOOST = 1.5
+CH4_BOOST = 0.5
 
-#images
-ch2 = Image.open("cha.png").convert("L")
-ch4 = Image.open("chb.png").convert("L")
 
-#ch4 = PIL.ImageOps.invert(ch4)
-#ch4 = PIL.ImageOps.autocontrast(ch4)
-ch2 = PIL.ImageOps.autocontrast(ch2)
+def parse_args() -> dict:
+    parser = argparse.ArgumentParser(description='NOAA APT Colorizer CLI', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-m', '--mode', type=str, default='noir', help='The mode, can be "ir", "ir_night", or "sunset"')
+    parser.add_argument('-b', '--boost', action='store_true', help='Boost the land color on very blue images')
+    parser.add_argument('cha', help='Source of channel A')
+    parser.add_argument('chb', help='Source of channel B')
+    args = parser.parse_args()
 
-#Variables
-deyellowfactor = 100
-black_pixels = []
-ch2_blend = 1
-ch4_blend = 200
-ch2_boost = 1.5
-ch4_boost = 0.5
+    return vars(args)
 
-#convert to arrays
-ch2_array = ch2.load()
-ch4_array = ch4.load()
 
-#sizes
-xsize,ysize = ch2.size
+def get_images_arrays(arguments: dict) -> [array, array]:
+    ch2 = Image.open(arguments['cha']).convert('L')
+    ch2 = ImageOps.autocontrast(ch2)
+    ch2_array = ch2.load()
 
-#output
-outimg = Image.new('RGB',(xsize,ysize))
+    ch4 = Image.open(arguments['chb']).convert('L')
+    ch4_array = ch4.load()
 
-def change_contrast(img, level):
-    factor = (259 * (level + 255)) / (255 * (259 - level))
-    def contrast(c):
-        return 128 + factor * (c - 128)
-    return img.point(contrast)
+    return ch2.size, ch2_array, ch4_array
 
-#main loop
-"""
-for y in range(ysize):
-    for x in range(xsize):
-        
-        ch4.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
-"""
-if args[1] == "-ir":
-    for y in range(ysize):
-        for x in range(xsize):
-            if ch2_array[x,y] - ch4_array[x,y] <= -50:
-                outimg.putpixel((x,y),(ch4_array[x,y],ch4_array[x,y],int(1*ch4_array[x,y])))
-            else:
-                outimg.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
-elif args[1] == "-ir_night":
-    for y in range(ysize):
-        for x in range(xsize):
-            if ch2_array[x,y] - ch4_array[x,y] <= -40:
-                outimg.putpixel((x,y),(round(ch2_array[x,y]*0.8 + ch4_array[x,y]*0.2),round(ch2_array[x,y]*0.8 + ch4_array[x,y]*0.2),round(ch2_array[x,y]*0.8 + ch4_array[x,y]*0.2)))
-                #black_pixels.append([x,y])
-            else:
-                    outimg.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
 
-elif args[1] == "-sunset":
-    for y in range(ysize):
-        for x in range(xsize):
-            if ch2_array[x,y] - ch4_array[x,y] <= -40:
-                outimg.putpixel((x,y),(round(ch2_array[x,y]*ch2_blend),round(ch2_array[x,y]*ch2_blend),round(ch4_array[x,y]*ch2_array[x,y]/ch4_blend)))
-                #black_pixels.append([x,y])
-            else:
-                    outimg.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
+def colour(ch2_array: array, ch4_array: array, size: tuple, arguments: dict) -> Image:
+    xsize, ysize = size
+    outimg = Image.new('RGB', (xsize, ysize))
 
-else:
-    for y in range(ysize):
-        for x in range(xsize):
-            outimg.putpixel((x,y),(ch2_array[x,y],ch2_array[x,y],int(1*ch4_array[x,y])))
-if args[2] == "-boost":
-    outimg_array = outimg.load()
-    for y in range(ysize):
-        for x in range(xsize):
-            outimg.putpixel((x,y),(round(outimg_array[x,y][0]*ch2_boost),round(outimg_array[x,y][1]*ch2_boost),round(outimg_array[x,y][2]*ch4_boost)))
-            
+    match arguments['mode']:
+        case 'ir':
+            for y in range(ysize):
+                for x in range(xsize):
+                    if (ch2_array[x, y] - ch4_array[x, y]) <= -50:
+                        outimg.putpixel((x, y), (ch4_array[x, y], ch4_array[x, y], int(1 * ch4_array[x, y])))
+                    else:
+                        outimg.putpixel((x, y), (ch2_array[x, y], ch2_array[x, y], int(1 * ch4_array[x, y])))
+        case 'ir_night':
+            for y in range(ysize):
+                for x in range(xsize):
+                    if (ch2_array[x,y] - ch4_array[x,y]) <= -40:
+                        outimg.putpixel((x, y), (round(ch2_array[x, y] * 0.8 + ch4_array[x,y] * 0.2),
+                                                 round(ch2_array[x,y] * 0.8 + ch4_array[x,y] * 0.2),
+                                                 round(ch2_array[x,y] * 0.8 + ch4_array[x,y] * 0.2)))
+                    else:
+                        outimg.putpixel((x, y), (ch2_array[x, y], ch2_array[x, y], int(1 * ch4_array[x, y])))
+        case 'sunset':
+            for y in range(ysize):
+                for x in range(xsize):
+                    if (ch2_array[x,y] - ch4_array[x,y]) <= -40:
+                        outimg.putpixel((x, y), (round(ch2_array[x, y] * CH2_BLEND),
+                                                 round(ch2_array[x, y] * CH2_BLEND),
+                                                 round(ch4_array[x, y] * ch2_array[x, y] / CH4_BLEND)))
+                    else:
+                        outimg.putpixel((x, y), (ch2_array[x, y], ch2_array[x, y], int(1 * ch4_array[x, y])))
+        case 'noir':
+            for y in range(ysize):
+                for x in range(xsize):
+                    outimg.putpixel((x, y), (ch2_array[x, y], ch2_array[x, y], int(1 * ch4_array[x, y])))
+        case _:
+            raise Exception('Modes supported are "ir", "ir_night", and "sunset"')
 
-outimg = PIL.ImageOps.autocontrast(outimg)
-#outimg = change_contrast(outimg,100)
+    return outimg
 
-outimg.show()
-outimg.save("color.png")
+
+def boost(image: Image, size: array, arguments: dict) -> Image:
+    if arguments['boost']:
+        xsize, ysize = size
+        outimg_array = image.load()
+
+        for y in range(ysize):
+            for x in range(xsize):
+                image.putpixel((x, y), (round(outimg_array[x, y][0] * CH2_BOOST),
+                                        round(outimg_array[x, y][1] * CH2_BOOST),
+                                        round(outimg_array[x, y][2] * CH4_BOOST)))
+    
+    return image
+
+
+def export_image(image: Image):
+    out_image = ImageOps.autocontrast(image)
+
+    out_image.show()
+    out_image.save('color.png')
+
+
+if __name__ == '__main__':
+    args: dict = parse_args()
+    size, ch2, ch4 = get_images_arrays(args)
+    image = colour(ch2, ch4, size, args)
+    image = boost(image, size, args)
+
+    export_image(image)
